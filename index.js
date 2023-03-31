0d625bcbbc2ce12b7aa794e4dff57c391c09c045
AnjaClara <anaclaraguimaraes1@gmail.com>
2023-03-31 11:41:00 -0300

    sequelize

--- index.js
+++ index.js
@@ -4,25 +4,35 @@
   const Dados = require('./dados');
   await database.sync();
 
-  await Dados.create({
-    nome: 'João Silva',
-    email: 'joao.silva@example.com',  
-  })
+  //// EX6!!!
+  // await Dados.destroy({ where: { id: 19 }});
 
-  await Dados.create({
-    nome: 'Maria Santos',
-    email: 'maria.santos@example.com',
-  });
+  //// EX5!!!
+  // await Dados.update({
+  //   nome: 'Ana Clara',
+  //   email: 'ana.clara@example.com',
+  // }, { where: { id: 17 }});
+
+  /// EX4!!!
+  // await Dados.create({
+  //   nome: 'João Silva',
+  //   email: 'joao.silva@example.com',  
+  // })
+
+  // await Dados.create({
+  //   nome: 'Maria Santos',
+  //   email: 'maria.santos@example.com',
+  // });
   
-  await Dados.create({
-    nome: 'Karina Oliveira',
-    email: 'ka.oliveira@example.com',
-  });
+  // await Dados.create({
+  //   nome: 'Karina Oliveira',
+  //   email: 'ka.oliveira@example.com',
+  // });
   
-  await Dados.create({
-    nome: 'Ana Souza',
-    email: 'ana.souza@example.com',
-  });
+  // await Dados.create({
+  //   nome: 'Ana Souza',
+  //   email: 'ana.souza@example.com',
+  // });
 
   const dados = await Dados.findAll();
 
